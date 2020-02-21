85a9862b339d3825e45aa28a3f9861088fa99c7a
User <glagakis@gmail.com>
2020-02-21 19:47:11 +0200

    δουλευει κανονικά με τους φακέλους

--- WindowsApp1/Form1.vb
+++ WindowsApp1/Form1.vb
@@ -1,27 +1,42 @@
 ﻿Imports System.IO
 Imports System.Net
 Imports System.Threading.Thread
+
 Imports System.IO.Compression
+
+
+
 Imports System.Net.WebClient
 Public Class Form1
     Dim th1 As Threading.Thread
     Dim gPath As String = "c:\mercvb\updates"
-    Dim gVerServ As String
-    Dim gVerWork As String
+    Dim gVerServ As String = ""
+    Dim gVerWork As String = ""
+
+
+
+
+
+
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles download.Click
 
+
+        'δημιουργει τον καταλογο της παλιάς εκδοσης και κανει copy to updw.txt & mercury.exe
         If Directory.Exists(gPath + "\" + gVerWork) Then
         Else
             Directory.CreateDirectory(gPath + "\" + gVerWork)
         End If
-        If File.Exists(gPath + "\m2.exe") Then
-            If File.Exists(gPath + "\" + gVerWork + "\m2.exe") Then
+
+        ' μετακινει το mercury ston old version folder
+        If File.Exists(gPath + "\mercury.exe") Then
+            If File.Exists(gPath + "\" + gVerWork + "\mercury.exe") Then
             Else
-                File.Move(gPath + "\m2.exe", gPath + "\" + gVerWork + "\m2.exe")
+                File.Move(gPath + "\mercury.exe", gPath + "\" + gVerWork + "\mercury.exe")
             End If
 
         End If
 
+        ' μετακινει το updw.txt ston old version folder
         If File.Exists(gPath + "\updw.txt") Then
             If File.Exists(gPath + "\" + gVerWork + "\updw.txt") Then
             Else
@@ -29,12 +44,20 @@ Public Class Form1
             End If
 
         End If
+
+
+        ' kanei copy to upd.txt sto c:\mercvb\updates\updw.txt
         If File.Exists(gPath + "\upd.txt") Then
 
-            If File.Exists(gPath + "\" + gVerWork + "\updw.txt") Then
-                File.Delete(gPath + "\" + gVerWork + "\updw.txt")
+            If File.Exists(gPath + "\updw.txt") Then
+                File.Delete(gPath + "\updw.txt")
             End If
-            File.Copy(gPath + "\upd.txt", gPath + "\" + gVerWork + "\updw.txt")
+            File.Copy(gPath + "\upd.txt", gPath + "\updw.txt")
+        End If
+
+        'σβηνει το παλιό zip για να έρθει το νέο
+        If File.Exists(gPath + "\mercury.zip") Then
+            File.Delete(gPath + "\mercury.zip")
         End If
 
 
@@ -42,6 +65,8 @@ Public Class Form1
         Timer1.Enabled = True
 
         ProgressBar1.Maximum = 14
+
+        'ανοιγει thread και κατεβάζει το mercury.zip
         th1 = New Threading.Thread(AddressOf down)
         th1.Start()
 
@@ -57,39 +82,29 @@ Public Class Form1
         Dim CLIENT As WebClient = New WebClient()
         CLIENT.Credentials = New NetworkCredential("mercury", "mercury12345678!!!")
 
-        '  CLIENT.DownloadFile( ftp://62.103.69.140:50001/mercury/update.inf", gPath + "\upd.inf")
+        '  CLIENT.DownloadFile( ftp://62.103.69.140:50001/update.inf", gPath + "\upd.inf")  ftp://62.103.69.140:50001/mercury/update.inf"
 
 
         CLIENT.DownloadFile(
-          "ftp://62.103.69.140:50001/mercfcon.exe", gPath + "\merc.zip")
-        MsgBox("ολοκληρώθηκε")
-        th1.Abort()
+            "ftp://192.168.1.100:50001/mercury.zip", gPath + "\mercury.zip")
+        '   "ftp://62.103.69.140:50001/mercury.zip", gPath + "\mercury.zip")
+        ' MsgBox("ολοκληρώθηκε")
+        ' th1.Abort()
         UnZip()
 
-        ProgressBar1.Value = ProgressBar1.Maximum
 
 
-    End Sub
 
 
-    'Declare the shell object
-    Dim shObj As Object = Activator.CreateInstance(Type.GetTypeFromProgID("Shell.Application"))
 
-    Sub UnZip()
 
-        'Create directory in which you will unzip your items.
-        'IO.Directory.CreateDirectory(outputFolder)
-
-        'Declare the folder where the items will be extracted.
-        Dim output As Object = shObj.NameSpace((gPath))
-
-        'Declare the input zip file.
-        Dim input As Object = shObj.NameSpace((gPath + "\merc.zip"))
-
-        'Extract the items from the zip file.
-        output.CopyHere((input.Items), 4)
+        '  ProgressBar1.Value = ProgressBar1.Maximum
+        ' th1.Abort()
+        MsgBox("ολοκληρώθηκε")
 
     End Sub
+
+
 
 
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
@@ -111,6 +126,7 @@ Public Class Form1
         ProgressBar1.Value = 0
         Try
             File.Delete(gPath + "\upd.txt")
+            ' File.Delete(gPath + "\mercury.exe")
         Catch ex As Exception
 
         End Try
@@ -122,7 +138,8 @@ Public Class Form1
             CLIENT.Credentials = New NetworkCredential("mercury", "mercury12345678!!!")
 
             CLIENT.DownloadFile(
-    "ftp://62.103.69.140:50001/mercury/update.inf", gPath + "\upd.txt")
+                 "ftp://192.168.1.100:50001/mercury/update.inf", gPath + "\upd.txt")
+            '  "ftp://62.103.69.140:50001/mercury/update.inf", gPath + "\upd.txt")
 
 
             '   .CLIENT.DownloadFile(
@@ -144,6 +161,8 @@ Public Class Form1
             FileClose(1)
         Else
             mes.Text = "Δεν εγινε σύνδεση με Κεντρικό"
+            download.Enabled = False
+
             Exit Sub
 
         End If
@@ -156,6 +175,8 @@ Public Class Form1
             FileClose(1)
             If gVerServ > gVerWork Then
                 mes.Text = "υπαρχει νέα έκδοση " + gVerServ
+                '.Enabled = True
+
 
             Else
                 mes.Text = "Εχετε την τελευταία έκδοση"
@@ -165,7 +186,7 @@ Public Class Form1
 
 
         Else
-            mes.Text = "νεα έκοδοση "
+            mes.Text = "υπαρχει νέα έκδοση " + gVerServ
 
 
         End If
@@ -174,4 +195,56 @@ Public Class Form1
 
 
     End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs)
+        UnZip()
+
+    End Sub
+
+    'Declare the shell object
+    '  Dim shObj As Object = Activator.CreateInstance(Type.GetTypeFromProgID("Shell.Application"))
+
+    Sub UnZip()
+
+
+        ' Dim startPath As String = "c:\example\start"
+        Dim zipPath As String = gPath + "\mercury.zip" '"c:\example\result.zip"
+        Dim extractPath As String = gPath ' "c:\example\extract"
+
+        If File.Exists(gPath + "\mercury.exe") Then
+            File.Delete(gPath + "\mercury.exe")
+        End If
+        ZipFile.ExtractToDirectory(zipPath, extractPath)
+
+
+        ' εαν υπάρχει σίγουρα στον φάκελο , σβησε το c:\mercvb\mercury.exe και αντέγραψε στην θέση του το νέο exe
+        If File.Exists(gPath + "\mercury.exe") Then
+
+            For Each proc As Process In Process.GetProcessesByName("mercury")
+                proc.Kill()
+                Sleep(2000)
+            Next
+
+
+
+
+
+            If File.Exists("c:\mercvb\mercury.exe") Then
+                File.Delete("c:\mercvb\mercury.exe")
+
+            End If
+
+
+            File.Copy(gPath + "\mercury.exe", "c:\mercvb\mercury.exe")
+        End If
+
+
+    End Sub
+
+
+
+
+
+
+
 End Class
