8ed1318902eb419143eadef7e7ba174e7a0d57ed
User <glagakis@gmail.com>
2020-02-21 06:08:23 +0200

    updater ενημερωση mercury apo FTP

---
+++ WindowsApp1/Form1.Designer.vb
@@ -0,0 +1,80 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.download = New System.Windows.Forms.Button()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.mes = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'download
+        '
+        Me.download.Location = New System.Drawing.Point(305, 84)
+        Me.download.Name = "download"
+        Me.download.Size = New System.Drawing.Size(303, 23)
+        Me.download.TabIndex = 0
+        Me.download.Text = "Λήψη έκδοσης"
+        Me.download.UseVisualStyleBackColor = True
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(305, 184)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(303, 23)
+        Me.ProgressBar1.TabIndex = 1
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 500
+        '
+        'mes
+        '
+        Me.mes.AutoSize = True
+        Me.mes.Location = New System.Drawing.Point(35, 9)
+        Me.mes.Name = "mes"
+        Me.mes.Size = New System.Drawing.Size(10, 13)
+        Me.mes.TabIndex = 4
+        Me.mes.Text = " "
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.mes)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.download)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents download As Button
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents mes As Label
+End Class
---
+++ WindowsApp1/Form1.vb
@@ -0,0 +1,143 @@
+﻿Imports System.IO
+Imports System.Net
+Imports System.Threading.Thread
+Imports System.IO.Compression
+Imports System.Net.WebClient
+Public Class Form1
+    Dim th1 As Threading.Thread
+    Dim gPath As String = "c:\mercvb\updates"
+    Dim gVerServ As String
+    Dim gVerWork As String
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles download.Click
+
+        If Directory.Exists(gPath + "\" + gVerWork) Then
+        Else
+            Directory.CreateDirectory(gPath + "\" + gVerWork)
+        End If
+        If File.Exists(gPath + "\m2.exe") Then
+            File.Move(gPath + "\m2.exe", gPath + "\" + gVerWork + "\m2.exe")
+        End If
+
+        If File.Exists(gPath + "\updw.txt") Then
+            File.Move(gPath + "\updw.txt", gPath + "\" + gVerWork + "\updw.txt")
+        End If
+
+
+
+
+        Timer1.Enabled = True
+
+        ProgressBar1.Maximum = 14
+        th1 = New Threading.Thread(AddressOf down)
+        th1.Start()
+
+
+
+
+
+
+    End Sub
+
+    Sub down()
+
+        Dim CLIENT As WebClient = New WebClient()
+        CLIENT.Credentials = New NetworkCredential("mercury", "mercury12345678!!!")
+
+        '  CLIENT.DownloadFile( ftp://62.103.69.140:50001/mercury/update.inf", gPath + "\upd.inf")
+
+
+        CLIENT.DownloadFile(
+          "ftp://62.103.69.140:50001/mercfcon.exe", gPath + "\m2.exe")
+        MsgBox("ολοκληρώθηκε")
+        th1.Abort()
+        ProgressBar1.Value = ProgressBar1.Maximum
+
+
+    End Sub
+
+
+
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If File.Exists(gPath + "\m2.exe") Then
+
+            Dim myFile As New FileInfo(gPath + "\m2.exe")
+            Dim sizeInBytes As Long = myFile.Length
+            Me.Text = Format(sizeInBytes / 1000000, "##0.00")
+            ProgressBar1.Value = sizeInBytes / 1000000
+
+
+        End If
+
+    End Sub
+
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ProgressBar1.Value = 0
+        Try
+            File.Delete(gPath + "\upd.txt")
+        Catch ex As Exception
+
+        End Try
+
+
+        Try
+
+            Dim CLIENT As WebClient = New WebClient()
+            CLIENT.Credentials = New NetworkCredential("mercury", "mercury12345678!!!")
+
+            CLIENT.DownloadFile(
+    "ftp://62.103.69.140:50001/mercury/update.inf", gPath + "\upd.txt")
+
+
+            '   .CLIENT.DownloadFile(
+            ' "ftp://62.103.69.140:50001/mercfcon.exe", gPath + "\m2.exe")
+            ' MsgBox("ολοκληρώθηκε")
+            '   th1.Abort()
+            ' ProgressBar1.Value = ProgressBar1.Maximum
+
+        Catch ex As Exception
+
+        End Try
+
+
+
+        If File.Exists(gPath + "/upd.txt") Then
+            FileOpen(1, gPath + "/upd.txt", OpenMode.Input)
+            gVerServ = LineInput(1)
+
+            FileClose(1)
+        Else
+            mes.Text = "Δεν εγινε σύνδεση με Κεντρικό"
+            Exit Sub
+
+        End If
+
+
+        If File.Exists(gPath + "/updw.txt") Then
+            FileOpen(1, gPath + "/updw.txt", OpenMode.Input)
+            gVerWork = LineInput(1)
+
+            FileClose(1)
+            If gVerServ > gVerWork Then
+                mes.Text = "υπαρχει νέα έκδοση " + gVerServ
+
+            Else
+                mes.Text = "Εχετε την τελευταία έκδοση"
+            End If
+
+
+
+
+        Else
+            mes.Text = "νεα έκοδοση "
+
+
+        End If
+
+
+
+
+    End Sub
+End Class
